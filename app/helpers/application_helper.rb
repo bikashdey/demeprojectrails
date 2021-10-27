df4fe92ea5997923c33481e005ec1aba7d77f987
Bikash <bikashdey200@gmail.com>
2021-10-27 19:53:18 +0530

    conterted sqlite3 to mysql

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,9 @@
 module ApplicationHelper
 
     def find_user(user_id)
-   
         sql = "SELECT * FROM users WHERE id = #{user_id}"
         @users = ActiveRecord::Base.connection.exec_query(sql)
-        
-    
+          
     end
 
 end
